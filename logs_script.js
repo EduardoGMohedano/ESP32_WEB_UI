a8b2cfbdbfa0632d6c8b72167d746eb9fa2e49d6
Eduardo Mohedano <egarciamohedano@gmail.com>
2024-02-15 15:26:11 -0600

    chore (download): add http request to download logs file.txt

--- logs_script.js
+++ logs_script.js
@@ -31,3 +31,27 @@ document.getElementById('loadLogsButton').addEventListener('click', function() {
 document.getElementById('clearLogsButton').addEventListener('click', function() {
     document.getElementById('messageBox').innerHTML = '';
 });
+
+
+document.getElementById('downloadLogsButton').addEventListener('click', function() {
+    // Replace 'ws://example.com/websocket' with your WebSocket server URL
+    const fileUrl = 'http://192.168.3.44/logs.txt';
+
+   // Create a new <a> element
+   const downloadLink = document.createElement('a');
+
+   // Set the href attribute to the file URL
+   downloadLink.href = fileUrl;
+
+   // Optionally, set the download attribute to the desired file name
+   downloadLink.download = 'Logs.txt';
+
+   // Append the <a> element to the body (it doesn't have to be visible)
+   document.body.appendChild(downloadLink);
+
+   // Programmatically trigger a click on the <a> element
+   downloadLink.click();
+
+   // Remove the <a> element after triggering the download
+   document.body.removeChild(downloadLink); 
+});
